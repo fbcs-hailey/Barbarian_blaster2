394b808e58be4bc07fc55fd0500e5b33b1dc14f3
fbcs-hailey <2260541@flourbluffschools.org>
2026-01-20 12:18:08 -0600

    add health and update base

--- home/home.gd
+++ home/home.gd
@@ -1,5 +1,18 @@
 extends Node3D
 class_name Home
 
+@export var max_health:=5
+@onready var label_3d: Label3D = $Label3D
+
+var health: int :
+	set(new_health):
+		health=new_health
+		label_3d.text=str(health)+"/"+str(max_health)
+
+func _ready() -> void:
+	health=max_health
+
 func take_damage()-> void:
 	print("damage taken at home")
+	health-=1
+	
