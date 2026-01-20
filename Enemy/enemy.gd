dca1d4cf5e4b7a9f55ab545f6c66990c6db9120c
fbcs-hailey <2260541@flourbluffschools.org>
2026-01-20 11:57:21 -0600

    create home

--- Enemy/enemy.gd
+++ Enemy/enemy.gd
@@ -2,7 +2,7 @@ extends PathFollow3D
 
 ## The speed in m/s the enemy will move
 @export_range(0,100) var speed:int=10
-
+@onready var home : Home =get_tree().get_first_node_in_group('home')
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,3 +12,6 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	progress+=delta*speed
+	if progress_ratio==1.0:
+		home.take_damage()
+		queue_free()
