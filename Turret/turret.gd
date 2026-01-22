a14aa8b52b2f7a8686f330fbbe5a66a404079f0b
fbcs-hailey <2260541@flourbluffschools.org>
2026-01-22 12:41:10 -0600

    path

--- Turret/turret.gd
+++ Turret/turret.gd
@@ -1,8 +1,14 @@
 extends Node3D
 
 @export var projectile:PackedScene
+var enemy_path:Path3D
+
+func _physics_process(delta: float) -> void:
+	var enemy=enemy_path.get_children().back
+	look_at(enemy.global_position,Vector3.UP,true)
 
 func _on_timer_timeout() -> void:
 	var new_projectile:=projectile.instantiate()
 	add_child(new_projectile)
 	new_projectile.global_position=global_position
+	
--- Turret/turret_manager.gd
+++ Turret/turret_manager.gd
@@ -1,9 +1,12 @@
 extends Node3D
 class_name TurretManager
 
+@export var enemy_path:Path3D
+
 @export var turret:PackedScene
 
 func build_turret(place:Vector3)->void:
 	var new_turret:=turret.instantiate()
 	add_child(new_turret)
 	new_turret.global_position = place
+	new_turret.enemy_path=enemy_path
