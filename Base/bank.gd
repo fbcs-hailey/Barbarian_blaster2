bb084f4e391ed0685558fda27f91fc6e33da2518
fbcs-hailey <2260541@flourbluffschools.org>
2026-01-26 13:52:26 -0600

    add gold

---
+++ Base/bank.gd
@@ -0,0 +1,12 @@
+extends MarginContainer
+
+@export var starting_gold:=150
+@onready var label: Label = $Label
+
+var gold:int:
+	set(new_gold):
+		gold=max(0,new_gold)
+		label.text="Gold: "+str(gold)
+
+func _ready() -> void:
+	gold=starting_gold
