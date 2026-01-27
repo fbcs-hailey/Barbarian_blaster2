48cdb5bb84289a07f1543e65e7172de3d9d42f06
fbcs-hailey <2260541@flourbluffschools.org>
2026-01-27 12:39:12 -0600

    undone

--- Base/path_3d.gd
+++ Base/path_3d.gd
@@ -1,10 +1,13 @@
 extends Path3D
 
 @export var enemy_scene:PackedScene
+@export var difMan:Node
+@onready var timer: Timer = $Timer
 
 func spawn_enemy():
 	var cur_enemy=enemy_scene.instantiate()
 	add_child(cur_enemy)
+	timer.wait_time=difMan.get_spawn_time()
 
 func _on_timer_timeout() -> void:
 	spawn_enemy()
---
+++ manager/difficulty_manager.gd
@@ -0,0 +1,21 @@
+extends Node
+
+@onready var timer: Timer = $Timer
+@export var game_length:=10.0
+@export var spawn_time_curve:Curve
+@export var enemy_time_curve:Curve
+
+func _ready() -> void:
+	timer.start(game_length)
+
+func _process(delta: float) -> void:
+	print(game_progress_ratio())
+
+func game_progress_ratio()->float:
+	return 1.0-(timer.time_left/game_length)
+
+func get_spawn_time()->float:
+	return spawn_time_curve.sample(game_progress_ratio())
+
+func get_enemy_health()->float:
+	return 0.0
